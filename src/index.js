0345cb18f5ef6b37f6718f957fefa4b551c51d9a
zoron <zoronlivingston@gmail.com>
2018-02-28 22:28:32 +0800

    scroll done

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 const aniDuration = Symbol('aniDuration');
-const currentSection = Symbol('currentSection');
+const currentSlide = Symbol('currentSlide');
 const doc = Symbol('doc');
 const handleMouseWheel = Symbol('handleMouseWheel');
 const lastAniTime = Symbol('lastAniTime');
 const option = Symbol('option');
+const scrollInSlide = Symbol('scrollInSlide');
 const win = Symbol('win');
 const wrapper = Symbol('wrapper');
 
@@ -15,17 +16,18 @@ class Scroll {
 
     self[option] = {
       aniDuration: `${defaultAniDuration}ms`,
-      container: null,
+      viewport: null,
       direction: 'vertical',
       idleTime: 200,
       loop: true,
       paginator: false,
-      sections: []
+      slides: []
     };
     self[aniDuration] = defaultAniDuration;
-    self[currentSection] = 0;
+    self[currentSlide] = 0;
     self[doc] = null;
     self[lastAniTime] = 0;
+    self[scrollInSlide] = 0;
     self[win] = null;
     self[wrapper] = null;
 
@@ -33,27 +35,26 @@ class Scroll {
       self[option][k] = opt[k];
     });
 
-    const sections = self[option].sections;
-    if (sections.length < 2) throw Error('sections must be more than 2');
-    sections.forEach(s => {
+    const slides = self[option].slides;
+    slides.forEach(s => {
       if (s instanceof Element) {
       } else throw Error('section must be an instance of Element');
     });
 
-    self[doc] = sections[0].ownerDocument;
+    self[doc] = slides[0].ownerDocument;
     self[win] = self[doc].defaultView;
 
-    let container = self[option].container;
-    if (container === null) {
-      container = self[doc].getElementByTagName('body')[0];
+    let viewport = self[option].viewport;
+    if (viewport === null) {
+      viewport = self[doc].getElementByTagName('body')[0];
     }
-    container.style.position = 'relative';
-    container.style.height = '100%';
-    container.style.overflow = 'hidden';
-    container.addEventListener('DOMMouseScroll', (e) => {
+    viewport.style.position = 'relative';
+    viewport.style.height = '100%';
+    viewport.style.overflow = 'hidden';
+    viewport.addEventListener('DOMMouseScroll', (e) => {
       self[handleMouseWheel](e);
     });
-    container.addEventListener('mousewheel', (e) => {
+    viewport.addEventListener('mousewheel', (e) => {
       self[handleMouseWheel](e);
     });
 
@@ -70,56 +71,114 @@ class Scroll {
       d.match(/^\d+$/) ?
       `${d}s` :
       '1s';
-    self[wrapper].style.transition = `top ${duration} ease 0s`;
+    self[wrapper].style.transition = `all ${duration} ease 0s`;
     self[aniDuration] = timeToMsNum(d);
 
-    sections.forEach(s => {
-      if (s.getAttribute('full')) {
-        s.style.flex = `0 0 ${container.clientHeight}`;
+    slides.forEach(s => {
+      if (s.getAttribute('full') === 'true') {
+        const originHeight = s.clientHeight;
+        s.style.flex = `0 0 ${Math.ceil(originHeight / viewport.clientHeight) * viewport.clientHeight}`;
       }
       moveEl(s, self[wrapper]);
     });
-    moveEl(self[wrapper], container);
+    moveEl(self[wrapper], viewport);
   }
 
   // PUBLIC
   scrollDown () {
     const self = this;
 
-    const sections = self[option].sections;
-    if (self[currentSection] === sections.length - 1 && !self[option].loop)
+    const slides = self[option].slides;
+    if (self._isLastSlide() && !self[option].loop)
       return;
 
+    const nextSlide = self._isLastSlide() ?
+      0 : self[currentSlide] + 1;
+    const multiPages = self._isSlideMutiPages(self[currentSlide]);
     const top = strToNum(self[wrapper].style.top);
-    self[wrapper].style.top =
-      self[currentSection] === sections.length - 1 ?
-      0 :
-      top - sections[self[currentSection]].clientHeight;
+    const newTopDiff = !multiPages ?
+      slides[self[currentSlide]].clientHeight :
+      (slides[self[currentSlide]].getAttribute('full') === 'true' ?
+      self[option].viewport.clientHeight :
+      slides[self[currentSlide]].clientHeight - self[scrollInSlide]);
 
-    if (++self[currentSection] > sections.length - 1) {
-      self[currentSection] = 0;
+    self[wrapper].style.top =
+      self._isLastSlide() ?
+      0 : top - newTopDiff;
+
+    if (!multiPages || (multiPages && self[scrollInSlide] + newTopDiff === slides[self[currentSlide]].clientHeight)) {
+      self[currentSlide] = nextSlide;
+      if (self[scrollInSlide] !== 0) self[scrollInSlide] = 0;
+    } else if (multiPages) {
+      self[scrollInSlide] += newTopDiff;
     }
   }
 
   scrollUp () {
     const self = this;
 
-    if (self[currentSection] === 0 && !self[option].loop)
+    if (self._isFirstSlide() && !self[option].loop)
       return;
 
-    const sections = self[option].sections;
+    const slides = self[option].slides;
+    const lastSlide = self._isFirstSlide() ?
+      slides.length - 1 : self[currentSlide] - 1;
+    const multiPages = self._isSlideMutiPages(self[currentSlide]);
     const top = strToNum(self[wrapper].style.top);
     self[wrapper].style.top =
-      self[currentSection] === 0 ?
-      sections[sections.length - 1].clientHeight - self[wrapper].clientHeight :
-      top + sections[self[currentSection] - 1].clientHeight;
+      self._isFirstSlide() ?
+      slides[slides.length - 1].clientHeight - self[wrapper].clientHeight :
+      (self[scrollInSlide] === 0 ?
+      top + slides[lastSlide].clientHeight :
+      top + self[scrollInSlide]);
 
-    if (--self[currentSection] < 0) {
-      self[currentSection] = sections.length - 1;
+    if (self[scrollInSlide] === 0) {
+      self[currentSlide] = lastSlide;
+    } else {
+      self[scrollInSlide] = 0;
+    }
+  }
+
+  toggleFull (el) {
+    if (el.getAttribute('full') === 'true') {
+      el.removeAttribute('full');
+      el.style.flex = '';
+    } else {
+      el.setAttribute('full', 'true');
+      el.style.flex = `0 0 ${this[option].viewport.clientHeight}`;
     }
   }
 
   // PRIVATE
+  _isFirstSlide () {
+    return this[currentSlide] === 0;
+  }
+
+  _isLastSlide () {
+    // console.log(this[currentSlide], this[option].slides.length);
+    return this[currentSlide] === this[option].slides.length - 1;
+  }
+
+  _isSlideOnTop () {
+    return strToNum(this[wrapper].style.top) === this._prevSlidesHeight();
+  }
+
+  _isSlideMutiPages (i) {
+    return this[option].slides[i].clientHeight > this[option].viewport.clientHeight;
+  }
+
+  _prevSlidesHeight () {
+    const self = this;
+    let index = 0;
+    return Array.from(self[option].slides).reduce((a, b) => {
+      return index++ === 0 ?
+        a.clientHeight :
+        (self[currentSlide] > index - 1 ?
+        a + b.clientHeight :
+        a);
+    });
+  }
+
   [handleMouseWheel] (e) {
     const self = this;
     const now = new Date().getTime();
