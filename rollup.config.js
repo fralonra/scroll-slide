277c835332087489a3079b45e72ec968c19a15f9
fralonra <zoronlivingston@gmail.com>
2020-05-07 15:07:29 +0800

    ci: build with babel

--- rollup.config.js
+++ rollup.config.js
@@ -1,20 +1,35 @@
+import babel from '@rollup/plugin-babel'
 import { terser } from 'rollup-plugin-terser'
 import filesize from 'rollup-plugin-filesize'
 
-export default [{
-  input: 'src/index.js',
-  plugins: [filesize()],
-  output: {
-    file: 'dist/scroll-slide.js',
-    format: 'umd',
-    name: 'Scroll'
+const babelOption = {
+  presets: [['@babel/env', { modules: false }]]
+}
+
+export default [
+  {
+    input: 'src/index.js',
+    plugins: [
+      babel(babelOption),
+      filesize()
+    ],
+    output: {
+      file: 'dist/scroll-slide.js',
+      format: 'umd',
+      name: 'Scroll'
+    }
+  },
+  {
+    input: 'src/index.js',
+    plugins: [
+      babel(babelOption),
+      terser(),
+      filesize()
+    ],
+    output: {
+      file: 'dist/scroll-slide.min.js',
+      format: 'umd',
+      name: 'Scroll'
+    }
   }
-}, {
-  input: 'src/index.js',
-  plugins: [terser(), filesize()],
-  output: {
-    file: 'dist/scroll-slide.min.js',
-    format: 'umd',
-    name: 'Scroll'
-  }
-}]
+]
